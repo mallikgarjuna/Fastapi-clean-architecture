e643e1e7a4261012f421fed3055f18eef5d7c4b1
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-01 10:15:55 +0200

    Add run.sh script to start postgres before running fastapi dev

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ===================================
+# Before running this script, do:
+# Make it executable
+# chmod +x run.sh (from the root dir)
+
+# Then run this script:
+# ./run.sh
+# ===================================
+
+# Ensure PostgreSQL is running (before running fastapi app)
+if systemctl is-active --quiet postgresql; then
+    echo "Starting PostgreSQL service..."
+    sudo systemctl start postgresql
+    sleep 3
+else
+    echo "PostgreSQL already running."
+fi
+
+# Run FastAPI app
+echo "Starting FastAPI app..."
+uv run fastapi dev app/main.py
