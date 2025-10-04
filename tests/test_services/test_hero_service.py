4cc2088d9ed20425b9498c0ff426fd3e0c1a23f9
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-04 21:19:54 +0200

    Add test for HeroService.read_many() in test_hero_service.py

--- tests/test_services/test_hero_service.py
+++ tests/test_services/test_hero_service.py
@@ -10,6 +10,7 @@ def test_create(mocker):
     #     db_hero = self.repo.create(db_hero)
     #     return db_hero
 
+    # Arrange
     repo_mock = mocker.Mock(spec=HeroRepository)
     service = HeroService(repo=repo_mock)
     fake_db_hero = Hero(
@@ -20,7 +21,32 @@ def test_create(mocker):
     repo_mock.create.return_value = fake_db_hero
 
     hero_in = HeroCreate(name="Deadpond", secret_name="Dive Wilson")
+    # Act
     result = service.create(hero_in)
 
+    # Assert
     assert result is fake_db_hero
     repo_mock.create.assert_called_once_with(fake_db_hero)
+
+
+def test_read_many(mocker):
+    """calls repo and returns list of heroes"""
+    # def read_many(self, offset: int = 0, limit: int = 100):
+    # heroes = self.repo.read_many(offset, limit)
+    # return heroes
+
+    # Arrange
+    repo_mock = mocker.Mock(spec=HeroRepository)
+    repo_mock.read_many.return_value = [
+        Hero(id=1, name="Deadpond", secret_name="Dive Wilson", age=None, gender=None),
+        Hero(id=2, name="Deadpond", secret_name="Dive Wilson", age=None, gender=None),
+    ]
+    service = HeroService(repo=repo_mock)
+
+    # Act
+    result = service.read_many(offset=0, limit=100)
+
+    # Assert
+    assert len(result) == 2
+    repo_mock.read_many.assert_called_once_with(0, 100)
+    assert isinstance(result, list)
