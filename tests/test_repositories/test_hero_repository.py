2689f8dbc3c9879b0ca4647b9b4cd34cffe70c68
Mallik Gurram <mallikgarjuna@gmail.com>
2025-10-05 06:04:09 +0200

    Add integration test for HeroRepository read_many() in test_hero_repository using pytest

--- tests/test_repositories/test_hero_repository.py
+++ tests/test_repositories/test_hero_repository.py
@@ -29,3 +29,27 @@ def test_create(session):
     assert session.get(Hero, result.id).name == "Deadpond"
     assert session.get(Hero, result.id).secret_name == "Dive Wilson"
     assert session.get(Hero, result.id).age is None  # b/c it was not set during create
+
+
+def test_read_many(session):
+    # def read_many(self, offset: int = 0, limit: int = 100):
+    #     heroes = self.session.exec(select(Hero).offset(offset).limit(limit)).all()
+    #     return heroes
+
+    # Arrange
+    repo = HeroRepository(session)
+
+    # create some rows in db
+    hero_db_1 = Hero(name="Deadpond1", secret_name="Dive Wilson 1")
+    hero_db_2 = Hero(name="Deadpond2", secret_name="Dive Wilson 2")
+    hero_db_3 = Hero(name="Deadpond3", secret_name="Dive Wilson 3")
+    repo.create(hero_db_1)
+    repo.create(hero_db_2)
+    repo.create(hero_db_3)
+
+    # Act
+    result = repo.read_many(offset=0, limit=100)
+
+    # Assert
+    assert isinstance(result, list)
+    assert len(result) == 3  # only 3 b/c a new session is created for every fixure call
